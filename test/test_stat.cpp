cd68f39dba6d474b4b295887ffaa3879473a8f84
leprik0n <diablo_soft@mail.ru>
2016-11-15 22:17:26 +0400

    Update test_stat.cpp

--- test/test_stat.cpp
+++ test/test_stat.cpp
@@ -3,9 +3,9 @@
 #include <iostream>
 #include <vector>
 #include <memory>
-#include <cstring>
-#include <random>
+#include <fstream>
 #include <algorithm>
+#include <cassert>
 using namespace std;
 
 typedef unique_ptr<Statistic> ptr_stat;
@@ -18,18 +18,32 @@ buf.push_back(ptr_stat(new MinStatistics));
 buf.push_back(ptr_stat(new MaxStatistics));
 buf.push_back(ptr_stat(new MeanStatistics));
 
-vector<double> rval(30);
-for_each(rval.begin(),rval.end(),[](double&i){return i = 5.16+rand()%32-20.25;});
-for_each(rval.begin(),rval.end(),[](const double&i){cout<<i<<endl;});
-cout<<endl;
-int s = rval.size();
-for(int i = 0; i < s; ++i){
+vector<double> test;
+ifstream is("test1.txt");
+
+for(double val;is>>val;){
    for(auto &st:buf)
-     st->process(rval[i]);
+     st->process(val);
+   test.push_back(val);
 }
+sort(test.begin(),test.end());
+for_each(test.begin(),test.end(),[](const double&i){cout<<i<<endl;});
 cout<<endl;
 for(auto &st:buf){
     cout<<st->eval()<<' ';
 }
+cout<<'\n'<<endl;;
+assert(buf[0]->eval() == test[0]);
+cout<<"TEST_1_OK"<<endl;
+assert(buf[1]->eval() == test[test.size()-1]);
+cout<<"TEST_2_OK"<<endl;
+double sum = 0;
+int s = test.size();
+for(int i = 0; i < s; ++i){
+    sum += test[i];
+}
+assert(buf[2]->eval() == sum/s);
+cout<<"TEST_3_OK"<<endl;
+
 return 0;
 }
