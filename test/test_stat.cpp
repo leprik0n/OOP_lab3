b3a8d9742c54663e5d84efa91989e5fa70fef09c
leprik0n <diablo_soft@mail.ru>
2016-11-15 21:56:23 +0400

    Update test_stat.cpp

--- test/test_stat.cpp
+++ test/test_stat.cpp
@@ -3,24 +3,33 @@
 #include <iostream>
 #include <vector>
 #include <memory>
+#include <cstring>
+#include <random>
+#include <algorithm>
 using namespace std;
 
 typedef unique_ptr<Statistic> ptr_stat;
 
-int main(int avgc,char**argv)
+int main()
 {
 vector<ptr_stat> buf;
+
 buf.push_back(ptr_stat(new MinStatistics));
 buf.push_back(ptr_stat(new MaxStatistics));
 buf.push_back(ptr_stat(new MeanStatistics));
 
-for(double val;cin>>val;){
+vector<double> rval(30);
+for_each(rval.begin(),rval.end(),[](double&i){return i = 5.16+rand()%32-20.25;});
+for_each(rval.begin(),rval.end(),[](const double&i){cout<<i<<endl;});
+cout<<endl;
+int s = rval.size();
+for(int i = 0; i < s; ++i){
    for(auto &st:buf)
-     st->process(val);
+     st->process(rval[i]);
 }
 cout<<endl;
-for(auto &s:buf)
-    cout<<s->eval()<<endl;
-
+for(auto &st:buf){
+    cout<<st->eval()<<' ';
+}
 return 0;
 }
