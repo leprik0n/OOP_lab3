06a6863a086e5e53afff983feb3e0c86aeff50d0
leprik0n <diablo_soft@mail.ru>
2016-11-22 22:27:18 +0400

    Update test_stat.cpp

--- test/test_stat.cpp
+++ test/test_stat.cpp
@@ -12,24 +12,18 @@ int main()
 {
 vector<unique_ptr<Statistic> > buf;
 buf.push_back(unique_ptr<Statistic>(new MinStatistics));
-/*buf.push_back(unique_ptr<Statistic>(new MaxStatistics));
+buf.push_back(unique_ptr<Statistic>(new MaxStatistics));
 buf.push_back(unique_ptr<Statistic>(new MeanStatistics));
 
 vector<double> test;
-ifstream is("test.txt");
-
+ifstream is("mas.txt");
+   
 for(double val;is>>val;){
    for(auto &st:buf)
      st->process(val);
    test.push_back(val);
 }
 sort(test.begin(),test.end());
-for_each(test.begin(),test.end(),[](const double&i){cout<<i<<endl;});
-cout<<endl;
-for(auto &st:buf){
-    cout<<st->eval()<<' ';
-}
-cout<<'\n'<<endl;;
 assert(buf[0]->eval() == test[0]);
 cout<<"TEST_1_OK"<<endl;
 assert(buf[1]->eval() == test[test.size()-1]);
@@ -40,14 +34,7 @@ for(int i = 0; i < s; ++i){
     sum += test[i];
 }
 assert(buf[2]->eval() == sum/s);
-cout<<"TEST_3_OK"<<endl;
-*/
-ifstream is("mas.txt");
-for(double val;is>>val;){
-   buf[0]->process(val);
-}
-cout<<buf[0]->eval()<<endl;
-cout<<"HELLO WORLD"<<endl;   
+cout<<"TEST_3_OK"<<endl; 
    
 return 0;
 }
