cad4c4938035c8b324758d55342f5907fad38a6d
leprik0n <diablo_soft@mail.ru>
2016-11-27 11:37:41 +0400

    Update stat.h

--- include/stat.h
+++ include/stat.h
@@ -1,37 +1,39 @@
 #pragma once
+
+#include <limits>
+
 class Statistic
 {
 public:
     virtual ~Statistic(){}
-    virtual void process(double)=0;
-    virtual double eval()const=0;
+    virtual void process(double) = 0;
+    virtual double eval() const = 0;
 };
 
 class MinStatistics:public Statistic
 {
 public:
     void process(double val);
-   	double eval()const;
+    double eval() const;
 private:
-    double min;
+    double min = std::numeric_limits<long double>::max();
 };
 
 class MaxStatistics:public Statistic
 {
 public:
-	void process(double val);
-	double eval()const;
+    void process(double val);
+    double eval() const;
 private:
-    double max;
+    double max = std::numeric_limits<long double>::min();
 };
 
 class MeanStatistics:public Statistic
 {
 public:
     void process(double val);
-    double eval()const;
+    double eval() const;
 private:
     double sum = 0;
     int count = 0;
 };
-
